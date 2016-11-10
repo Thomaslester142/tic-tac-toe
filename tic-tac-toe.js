8edc4314956a21045e0a4607c7bf72df79d9084e
ThomasLester <ThomasLester142@users.noreply.github.com>
2016-11-10 10:15:51 -0800

    Started Project

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,39 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+
+function topLeft() {
+
+}
+
+function topMid() {
+
+}
+
+function topRight() {
+
+}
+
+function midLeft() {
+
+}
+
+function midMid() {
+
+}
+
+function midRight() {
+
+}
+
+function botLeft() {
+
+}
+
+function botMid() {
+
+}
+
+function botRight() {
+
+}
